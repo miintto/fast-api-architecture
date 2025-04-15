58e94d99b5750a18d78ba1ba247c89cfd0514b6c
miintto <miintto.log@gmail.com>
2025-04-16 00:21:47 +0900

    feat: 미사용 메소드 제거

--- app/application/service/product.py
+++ app/application/service/product.py
@@ -1,9 +1,6 @@
 from fastapi import Depends, HTTPException
 
-from app.adapter.outbound.persistence.entity.product import (
-    Product,
-    ProductItem,
-)
+from app.adapter.outbound.persistence.entity.product import Product
 from app.adapter.outbound.persistence.product import ProductPersistenceAdapter
 from app.application.port.input import ProductUseCase
 
@@ -14,12 +11,6 @@ class ProductService(ProductUseCase):
         repo: ProductPersistenceAdapter = Depends(ProductPersistenceAdapter),
     ):
         self._repo = repo
-
-    def _calc_discount(self, item: ProductItem):
-        try:
-            return 100 - int(item.price * 100 / item.cost)
-        except ZeroDivisionError:
-            return 0
 
     async def get_product_list(self) -> list[Product]:
         return await self._repo.find_displayed_products()
